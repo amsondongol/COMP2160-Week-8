408c34b9042e71ef7c133026bef24207088d6d82
amsondongol <100895364+amsondongol@users.noreply.github.com>
2022-09-28 16:35:16 +1000

    Player created
    Player and collecting coins made

---
+++ Week 8/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField] float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+    transform.position += new Vector3(horizontal, 0, vertical) * Time.deltaTime * speed;
+    }
+}
