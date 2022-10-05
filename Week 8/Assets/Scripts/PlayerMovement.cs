1f5ef0fe0bae1cec4640dcec383109846d460a60
amsondongol <100895364+amsondongol@users.noreply.github.com>
2022-10-05 17:02:21 +1100

    Player 2 created
    Second player with different controller inputs added.

--- Week 8/Assets/Scripts/PlayerMovement.cs
+++ Week 8/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] float speed = 2.0f;
+    [SerializeField] int playerType=0;
+    public int PlayerType
+    {
+        get
+        {
+            return playerType;
+        }
+    }
+
+    float horizontal = 0f;
+    float vertical = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +27,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        if (playerType == 1)
+        {
+             horizontal = Input.GetAxis("Horizontal");
+             vertical = Input.GetAxis("Vertical");
+       }
+
+        if (playerType == 2)
+        {
+             horizontal = Input.GetAxis("Horizontal2");
+            vertical = Input.GetAxis("Vertical2");
+        }
+
         transform.position += new Vector3(horizontal, 0, vertical) * Time.deltaTime * speed;
+    }
+
+    public void SetPlayerType(int type)
+    {
+        playerType = type;
     }
 }
